3502fd2da56d36c38b242b1dc0e9e6d86705cf7a
AlexFedorko <alexfedorko15@gmail.com>
2021-06-22 15:38:45 +0300

    Fetch Users

---
+++ src/components/users/Users.js
@@ -0,0 +1,29 @@
+import {useEffect, useState} from 'react';
+
+export default function Users() {
+
+
+    let [usersList, setUsersList] = useState([]); // [.....user]
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(value => value.json())
+            .then(response => {
+                console.log(response);
+                setUsersList(response);
+            });
+    }, []);
+
+    return (
+        <div>
+            {
+                usersList
+                    .map(value => <div>{value.name}</div>)
+
+
+
+            }
+
+        </div>
+    );
+}
