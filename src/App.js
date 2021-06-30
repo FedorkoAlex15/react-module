528952f57a45f04d661b06a3dacd46afe3d06017
AlexFedorko <alexfedorko15@gmail.com>
2021-06-30 06:59:40 +0300

    reducer was trained

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
+import {BrowserRouter as Router, Link, Route, Switch} from "react-router-dom";
 
 function App() {
   return (
+      <Router>
       <div>
 
-      </div>
 
+
+              <div><Link to={'/users'}></Link>Users</div>
+              <div><Link to={'/posts'}></Link>Posts</div>
+              <div><Link to={'/comments'}></Link>Comments</div>
+
+
+              <Route path={'/users'} render={() => {
+                  return <div>It's users page</div>
+              }}/>
+
+              <Route path={'/posts'} >
+                  <div>It's posts</div>
+              </Route>
+
+
+
+
+
+      </div>
+      </Router>
   );
 }
 
