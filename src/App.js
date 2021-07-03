5ed4bcad05b33e6362c496cbacb0b6db0667e65c
AlexFedorko <alexfedorko15@gmail.com>
2021-07-03 08:12:18 +0300

    logic is written

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+import Users from "./users/Users";
 function App() {
   return (
-      <div>
+      <Router>
+          <div>
+              <Link to={'/users'}>users page</Link>
 
-      </div>
+
+              <Switch>
+                  <Route path={'/users'} component={Users}/>
+              </Switch>
+
+          </div>
+
+      </Router>
 
   );
 }
